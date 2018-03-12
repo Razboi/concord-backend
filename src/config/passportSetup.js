946ce6413c4ca63fcd6ba947dabc228b614b600a
Marc <recatala.marc@gmail.com>
2018-03-12 23:08:41 +0100

    Worked on implementing passport with google oauth

---
+++ src/config/passportSetup.js
@@ -0,0 +1,14 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./keys");
+
+passport.use(
+	new GoogleStrategy({
+		// api keys from google+
+		clientID: keys.google.clientID,
+		clientSecret: keys.google.clientSecret,
+		callbackURL: "http://localhost:3000"
+	}, () => {
+
+	})
+);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const socket = require("socket.io");
 const express = require("express");
+const passportSetup = require("./config/passportSetup");
+const passport = require("passport");
 
 const app = express();
+
+
+app.get("/oauth/google", passport.authenticate("google", {
+		scope: [ "profile" ]
+	}) );
+
 const server = app.listen( 8000 );
 const io = socket( server );
 
 io.on( "connection", client => {
-
 	client.username = "Anonymous";
 
 	client.on("changeUsername", username => {
@@ -14,12 +21,7 @@ io.on( "connection", client => {
 		client.username = username;
 	});
 
-	client.on("disconnect", () =>
-		console.log("User disconnected")
-	);
-
 	client.on("newMessage", message => {
 		io.sockets.emit("newMessage", client.username + ": " + message );
 	});
-
 });
